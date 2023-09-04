fb9f6eca2e0ee27fffbd290611b5224b1eff2c08
Mark <mark.arias7@gmail.com>
2023-09-04 13:13:34 -0500

    project in finished state.

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -11,45 +11,27 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    
     @IBOutlet weak var diceImageView1: UIImageView!
     
     @IBOutlet weak var diceImageView2: UIImageView!
     
-    var leftDiceNumber = 0
-    var rightDiceNumber = 5
-    
-    override func viewDidLoad() {
+    let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
         
-        super.viewDidLoad()
-    }
-    
     
     @IBAction func rollButtonPressed(_ sender: UIButton) {
-        // tip on how to get the image literal to appear
-        // type #imageLiteral()
-        // diceImageView1.image =  #imageLiteral(resourceName: "DiceFour")
-        // you can alternately  use code below as well
-        // diceImageView2.image = UIImage(named: "DiceTwo")
-    
-        if leftDiceNumber < 5 {
-            leftDiceNumber += 1
-        } else {
-            leftDiceNumber = 0
-        }
-        
-        if rightDiceNumber > 0 {
-            rightDiceNumber -= 1
-        } else {
-            rightDiceNumber = 5
-        }
-        
-        
-        diceImageView1.image =  [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][leftDiceNumber]
-        diceImageView2.image =  [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][rightDiceNumber]
-        
+         
+        diceImageView1.image =  diceArray.randomElement()
+        diceImageView2.image =  diceArray.randomElement()
     }
-    
 
 }
 
+/**
+ Notes:
+ 
+ tip on how to get the image literal to appear
+ type #imageLiteral()
+ diceImageView1.image =  #imageLiteral(resourceName: "DiceFour")
+ you can alternately  use code below as well
+ diceImageView2.image = UIImage(named: "DiceTwo")
+ */
