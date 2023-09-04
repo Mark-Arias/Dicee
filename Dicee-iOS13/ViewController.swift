b3f9ab5c4b738492fb052c741068f2ba2331c5c3
Mark <mark.arias7@gmail.com>
2023-09-03 22:56:33 -0500

    add in ability to roll dies incrementally. left die goes up in count. right die decrements.

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 
+// refactor this project to follow the MVC pattern!
+
 class ViewController: UIViewController {
     
     
@@ -14,24 +16,38 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var diceImageView2: UIImageView!
     
-    
+    var leftDiceNumber = 0
+    var rightDiceNumber = 5
     
     override func viewDidLoad() {
         
         super.viewDidLoad()
-        
-        // Do any additional setup after loading the view.
-        diceImageView1.image = UIImage(named: "DiceFour")
-        diceImageView2.image = UIImage(named: "DiceTwo")
-        
     }
     
     
     @IBAction func rollButtonPressed(_ sender: UIButton) {
         // tip on how to get the image literal to appear
         // type #imageLiteral()
-        diceImageView1.image =  #imageLiteral(resourceName: "DiceFour")
-        diceImageView2.image = #imageLiteral(resourceName: "DiceFour")
+        // diceImageView1.image =  #imageLiteral(resourceName: "DiceFour")
+        // you can alternately  use code below as well
+        // diceImageView2.image = UIImage(named: "DiceTwo")
+    
+        if leftDiceNumber < 5 {
+            leftDiceNumber += 1
+        } else {
+            leftDiceNumber = 0
+        }
+        
+        if rightDiceNumber > 0 {
+            rightDiceNumber -= 1
+        } else {
+            rightDiceNumber = 5
+        }
+        
+        
+        diceImageView1.image =  [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][leftDiceNumber]
+        diceImageView2.image =  [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][rightDiceNumber]
+        
     }
     
 
